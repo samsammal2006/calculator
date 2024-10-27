d228e91ca214b46941d99c537e79fcb3231b405d
samsammal2006 <samsammal08@gmail.com>
2024-10-27 22:31:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLastCharacter() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1); // Remove the last character
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
